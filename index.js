7671274a7f4d29b5ce29780520fcc36af749a809
tannv <tannv99@gmail.com>
2024-11-05 03:10:25 +0000

    Add index.js file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+   console.log("Nhan mot POST Request ve Homepage");
+   res.send('Hello POST');
+
+app.get('/list_user', function (req, res) {
+   console.log("Nhan mot GET Request ve /list_user");
+   res.send('Page Listing');
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
