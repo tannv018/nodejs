60114a07228111bc3c6b0dbb2cdcef77e0992633
tan018 <tannv99@gmail.com>
2024-11-06 07:40:25 +0000

    add index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/', function (req, res) {
-   console.log("Nhan mot POST Request ve Homepage");
-   res.send('Hello POST');
+// app.post('/', function (req, res) {
+//    console.log("Nhan mot POST Request ve Homepage");
+//    res.send('Hello POST');
 
-app.get('/list_user', function (req, res) {
-   console.log("Nhan mot GET Request ve /list_user");
-   res.send('Page Listing');
+// app.get('/list_user', function (req, res) {
+//    console.log("Nhan mot GET Request ve /list_user");
+//    res.send('Page Listing');
 
 
 app.listen(port, () => {
